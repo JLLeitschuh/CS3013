c3a81714f3e0bf1fc2c4e453469e8ad9a585431d
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2015-02-01 16:56:19 -0500

    Adds project 0 code for testing class syscalls

---
+++ jlleitschuh_project0/test.c
@@ -0,0 +1,22 @@
+#include <sys/syscall.h>
+#include <stdio.h>
+// These values MUST match the unistd_32.h modifications:
+#define __NR_cs3013_syscall1 355
+#define __NR_cs3013_syscall2 356
+#define __NR_cs3013_syscall3 357
+long testCall1 ( void) {
+	return (long) syscall(__NR_cs3013_syscall1);
+}
+long testCall2 ( void) {
+	return (long) syscall(__NR_cs3013_syscall2);
+}
+long testCall3 ( void) {
+	return (long) syscall(__NR_cs3013_syscall3);
+}
+int main () {
+	printf("The return values of the system calls are:\n");
+	printf("\tcs3013_syscall1: %ld\n", testCall1());
+	printf("\tcs3013_syscall2: %ld\n", testCall2());
+	printf("\tcs3013_syscall3: %ld\n", testCall3());
+	return 0;
+}
